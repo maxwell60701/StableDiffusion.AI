42f5a00cec1e85658682618193002d5dbc2f572d
mayiqun <maxwell.ma@lingshumed.com>
2024-06-03 17:57:10 +0800

    add httpclientfactory

---
+++ StableDiffusion.AI.Core/HttpClientBases/HttpClientBase.cs
@@ -0,0 +1,10 @@
+﻿namespace StableDiffusion.AI.Core.HttpClientBases
+{
+    internal static class HttpClientBase
+    {
+        internal static HttpClient GetClient(IHttpClientFactory httpClientFactory)
+        {
+            return httpClientFactory != null ? httpClientFactory.CreateClient() : new HttpClient();
+        }
+    }
+}
--- StableDiffusion.AI.Core/StableDiffusionService.cs
+++ StableDiffusion.AI.Core/StableDiffusionService.cs
@@ -5,9 +5,15 @@ namespace StableDiffusion.AI.Core
     public class StableDiffusionService
     {
         public StableDiffusionV1 V1 { get; private set; }
+
         public StableDiffusionService(string apiKey)
         {
             V1 = new StableDiffusionV1(apiKey);
+        }
+
+        public StableDiffusionService(string apiKey,IHttpClientFactory httpClientFactory)
+        {
+            V1 = new StableDiffusionV1(apiKey,httpClientFactory);
         }
     }
 }
